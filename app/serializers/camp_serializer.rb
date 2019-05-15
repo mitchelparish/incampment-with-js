e01dff52cc253b92d28977d08e9253676c0d5e38
mitchelparish <mitchelparish@gmail.com>
2019-05-15 11:40:26 -0600

    added serializer relationship

--- app/serializers/camp_serializer.rb
+++ app/serializers/camp_serializer.rb
@@ -1,3 +1,4 @@
 class CampSerializer < ActiveModel::Serializer
   attributes :id, :name, :state, :description, :created_at, :updated_at, :lat, :lng
+  
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :rating, :comments, :user_id, :camp_id, :created_at, :updated_at
+  belongs_to :camp
 end
